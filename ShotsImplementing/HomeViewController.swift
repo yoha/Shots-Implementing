80265fe6765d01288bad9658ce5786d701d01bfe
Yohannes Wijaya <readyforservice@icloud.com>
2016-02-29 23:09:25 +0700

    Basic Implementation in Swift: New Class File, Connecting the Class, IBOutlets

---
+++ ShotsImplementing/HomeViewController.swift
@@ -0,0 +1,43 @@
+//
+//  HomeViewController.swift
+//  ShotsImplementing
+//
+//  Created by Yohannes Wijaya on 2/29/16.
+//  Copyright © 2016 Yohannes Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    // MARK: - IBOutlet Properties
+    
+    @IBOutlet weak var userProfileButton: UIButton!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var backgroundMaskView: UIView!
+    @IBOutlet weak var dialogView: UIView!
+    @IBOutlet weak var popOverView: UIView!
+    @IBOutlet weak var shareSheetView: UIView!
+    
+    @IBOutlet weak var dialogHeaderView: UIView!
+    @IBOutlet weak var dialogImageButton: UIButton!
+    @IBOutlet weak var dialogHeaderTitleLabel: UILabel!
+    @IBOutlet weak var dialogHeaderAuthorLabel: UILabel!
+    @IBOutlet weak var dialogAvatarImageView: UIImageView!
+    @IBOutlet weak var dialogFavoritesLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var shareButton: UIButton!
+    
+    // MARK: - IBAction Methods
+    
+    @IBAction func imageButtonDidTouch(sender: UIButton) {
+    }
+    
+    @IBAction func likeButtonDidTouch(sender: UIButton) {
+    }
+    
+    @IBAction func shareButtonDidTouch(sender: UIButton) {
+        
+    }
+
+}
