710eeb7c4bc5d9dd25e7897e4be0f5a55f91a5b8
Yohannes Wijaya <readyforservice@icloud.com>
2016-03-12 20:08:26 +0700

    Spring Animations: Creating the Mask & The animation for the Share Dialog

--- ShotsImplementing/HomeViewController.swift
+++ ShotsImplementing/HomeViewController.swift
@@ -13,22 +13,38 @@ class HomeViewController: UIViewController {
     // MARK: - IBOutlet Properties
     
     @IBOutlet weak var userProfileButton: UIButton!
+    
     @IBOutlet weak var backgroundImageView: UIImageView!
     @IBOutlet weak var backgroundMaskView: UIView!
-    @IBOutlet weak var dialogView: UIView!
+   
     @IBOutlet weak var userPopoverView: UIView!
-    @IBOutlet weak var shareSheetView: UIView!
     
+    @IBOutlet weak var likeButton: UIButton!
+    
+    @IBOutlet weak var shareLabelsView: UIView!
+    @IBOutlet weak var shareSheetView: UIView!
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var emailButton: UIButton!
+    @IBOutlet weak var twitterButton: UIButton!
+    @IBOutlet weak var facebookButton: UIButton!
+    
+    @IBOutlet weak var maskButton: UIButton!
+    
+    @IBOutlet weak var dialogView: UIView!
     @IBOutlet weak var dialogHeaderView: UIView!
     @IBOutlet weak var dialogImageButton: UIButton!
     @IBOutlet weak var dialogHeaderTitleLabel: UILabel!
     @IBOutlet weak var dialogHeaderAuthorLabel: UILabel!
     @IBOutlet weak var dialogAvatarImageView: UIImageView!
     @IBOutlet weak var dialogFavoritesLabel: UILabel!
-    @IBOutlet weak var likeButton: UIButton!
-    @IBOutlet weak var shareButton: UIButton!
+    
+    
     
     // MARK: - IBAction Methods
+    
+    @IBAction func maskButtonDidTouch(sender: UIButton) {
+        self.hideShareView()
+    }
     
     @IBAction func userProfileButtonDidTouch(sender: UIButton) {
         self.userPopoverView.hidden = false
@@ -66,11 +82,29 @@ class HomeViewController: UIViewController {
     
     @IBAction func shareButtonDidTouch(sender: UIButton) {
         self.shareSheetView.hidden = false
-        self.shareSheetView.alpha = 0.0
+        self.shareSheetView.alpha = 0
         self.shareSheetView.transform = CGAffineTransformMakeTranslation(0, 234)
+        self.shareLabelsView.alpha = 0
+        self.emailButton.transform = CGAffineTransformMakeTranslation(0, 234)
+        self.twitterButton.transform = CGAffineTransformMakeTranslation(0, 234)
+        self.facebookButton.transform = CGAffineTransformMakeTranslation(0, 234)
+        self.maskButton.hidden = false
         SpringAnimation.spring(0.5) { [unowned self] () -> Void in
-            self.shareSheetView.alpha = 1.0
+            self.shareSheetView.alpha = 1
             self.shareSheetView.transform = CGAffineTransformMakeTranslation(0, 0)
+            self.dialogView.transform = CGAffineTransformMakeScale(0.8, 0.8)
+        }
+        SpringAnimation.springWithDelay(0.5, delay: 0.05) { [unowned self] () -> Void in
+            self.emailButton.transform = CGAffineTransformMakeTranslation(0, 0)
+        }
+        SpringAnimation.springWithDelay(0.5, delay: 0.10) { [unowned self] () -> Void in
+            self.twitterButton.transform = CGAffineTransformMakeTranslation(0, 0)
+        }
+        SpringAnimation.springWithDelay(0.5, delay: 0.15) { [unowned self] () -> Void in
+            self.facebookButton.transform = CGAffineTransformMakeTranslation(0, 0)
+        }
+        SpringAnimation.springWithDelay(0.5, delay: 0.20) { [unowned self] () -> Void in
+            self.shareLabelsView.alpha = 1
         }
     }
     
@@ -95,6 +129,18 @@ class HomeViewController: UIViewController {
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return .LightContent
+    }
+    
+    // MARK: - Helper Methods
+    
+    private func hideShareView() {
+        SpringAnimation.spring(0.5) { [unowned self] () -> Void in
+            self.shareSheetView.alpha = 0
+            self.shareSheetView.hidden = true
+            self.maskButton.hidden = true
+            self.shareSheetView.transform = CGAffineTransformMakeTranslation(0, 234)
+            self.dialogView.transform = CGAffineTransformMakeScale(1, 1)
+        }
     }
 
 }
